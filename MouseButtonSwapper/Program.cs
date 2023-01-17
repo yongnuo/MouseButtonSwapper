42fb7ef9100bbc5eb2bfded06db6fbb3c1be9179
Zacharias Jensen <zacharias.jensen@prevas.se>
2023-01-17 10:07:31 +0100

    First

---
+++ MouseButtonSwapper/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Threading;
+using SwapMouseButtons;
+
+Console.Title = "Mouse Button Swapper";
+
+var swapper = new Swapper();
+var result = swapper.Swap();
+var output = result
+	? "Buttons set to normal"
+	: "Buttons set to swapped (left hand)";
+Console.WriteLine(output);
+
+Thread.Sleep(3000);
---
+++ MouseButtonSwapper/Swapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SwapMouseButtons;
+
+public class Swapper
+{
+	[DllImport("user32.dll")]
+	public static extern bool SwapMouseButton(bool fSwap);
+	//-------------------------------
+	public bool Swap()
+	{
+		if (SwapMouseButton(true))
+		{
+			SwapMouseButton(false);
+			return true;
+		}
+
+		return false;
+	}
+}
